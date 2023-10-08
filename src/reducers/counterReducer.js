dc388854eb8a34ca92405b2cc874e7b9ddadefc2
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-10-08 18:49:23 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,8 @@
 const counterReducer = (state,action) =>{
-
+  switch(action.type){
+    case "Inc": return{...state, count: state.count+1}
+    case "Dec": return{...state, count: state.count-1}
+  }
 }
 
 export {counterReducer}
